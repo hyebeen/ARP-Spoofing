f29893271dde5fc9a9b89da648e02a0a223fe7ce
hyebeen <llhb3015@naver.com>
2016-05-08 00:24:33 +0900

    changes

--- arp_report/main.cpp
+++ arp_report/main.cpp
@@ -6,22 +6,22 @@
 #include <netinet/ether.h>
 #include <arpa/inet.h>
 
-struct arp_req {
-    u_int arp_hwtype[2];   // ARP 패킷의 하드웨어 타입
-    u_int arp_protype[2];  // ARP 패킷의 프로토콜 타입
-    u_int arp_hlen[1];   // ARP 패킷의 하드웨어 길이
-    u_int arp_plen[1];   // ARP 패킷의 프로토콜 길이
-    u_int arp_opr[2];   // ARP 패킷의 오퍼레이션
-    u_int arp_shwaddr[6];  // ARP 패킷의 소스MAC주소
-    u_int arp_sipaddr[4]; // ARP 패킷의 소스IP주소
-    u_int arp_thwaddr[6];   // ARP 패킷의 타겟MAC주소
-    u_int arp_tipaddr[4]; // ARP 패킷의 타겟IP주소
+struct eth_hdr {
+u_int8_t eth_dmac[6];   // ether destination mac주소
+u_int8_t eth_smac[6];  // ether source mac주소
+u_int16_t eth_type;   // ether type
 };
 
-struct eth_hdr {
-    u_int eth_dmac[6];   // ether destination mac주소
-    u_int eth_smac[6];  // ether source mac주소
-    u_int eth_type[2];   // ether type
+struct arp_req {
+    u_int16_t arp_hwtype;   // ARP 패킷의 하드웨어 타입
+    u_int16_t arp_protype;  // ARP 패킷의 프로토콜 타입
+    u_int8_t arp_hlen;   // ARP 패킷의 하드웨어 길이
+    u_int8_t arp_plen;   // ARP 패킷의 프로토콜 길이
+    u_int16_t arp_opr;   // ARP 패킷의 오퍼레이션
+    u_int8_t arp_shwaddr[6];  // ARP 패킷의 소스MAC주소
+    u_int8_t arp_sipaddr[4]; // ARP 패킷의 소스IP주소
+    u_int8_t arp_thwaddr[6];   // ARP 패킷의 타겟MAC주소
+    u_int8_t arp_tproaddr[4]; // ARP 패킷의 타겟IP주소
 };
 
 struct eth_arp_req {
@@ -39,34 +39,27 @@ int main()
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         return(2);
     }
-
     eth_hdr eth;
-    u_int eth_dmac[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+    u_char eth_dmac[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
     memcpy(eth.eth_dmac, eth_dmac, sizeof(eth_dmac));
-    u_int eth_smac[6] = {0x24, 0xf5, 0xaa, 0x75, 0xa4, 0xdf};
+    u_char eth_smac[6] = {0x24, 0xf5, 0xaa, 0x75, 0xa4, 0xdf};
     memcpy(eth.eth_smac, eth_smac, sizeof(eth_smac));
-    u_int eth_type[2] = {0x08, 0x06};
-    memcpy(eth.eth_type, eth_type, sizeof(eth_type));
+    eth.eth_type = htons(ETH_P_ARP);
 
     arp_req arp;
-    u_int arp_hwtype[2] = {0x00, 0x01};
-    memcpy(arp.arp_hwtype, arp_hwtype, sizeof(arp_hwtype));
-    u_int arp_protype[2] = {0x08, 0x00};
-    memcpy(arp.arp_protype, arp_protype, sizeof(arp_protype));
-    u_int arp_hlen[1] = {0x06};
-    memcpy(arp.arp_hlen, arp_hlen, sizeof(arp_hlen));
-    u_int arp_plen[1] = {0x04};
-    memcpy(arp.arp_plen, arp_plen, sizeof(arp_plen));
-    u_int arp_opr[2] = {0x00, 0x01};
-    memcpy(arp.arp_opr, arp_opr, sizeof(arp_opr));
-    u_int arp_shwaddr[6] = {0x24, 0xf5, 0xaa, 0x75, 0xa4, 0xdf};
+    arp.arp_hwtype = htons(ARPHRD_ETHER);
+    arp.arp_protype = htons(ETH_P_IP);
+    arp.arp_hlen = 6;
+    arp.arp_plen = 4;
+    arp.arp_opr = htons(ARPOP_REQUEST);
+    u_int8_t arp_shwaddr[6] = {0x24, 0xf5, 0xaa, 0x75, 0xa4, 0xdf};
     memcpy(arp.arp_shwaddr, arp_shwaddr, sizeof(arp_shwaddr));
-    u_int arp_sproaddr[4] = {0xc0, 0xa8, 0x00, 0x07};
+    u_int8_t arp_sproaddr[4] = {0xc0, 0xa8, 0x00, 0x07};
     memcpy(arp.arp_sipaddr, arp_sproaddr, sizeof(arp_sproaddr));
-    u_int arp_thwaddr[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    u_int8_t arp_thwaddr[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     memcpy(arp.arp_thwaddr, arp_thwaddr, sizeof(arp_thwaddr));
-    u_int arp_tproaddr[4] = {0xc0, 0xa8, 0x00, 0x01};
-    memcpy(arp.arp_tipaddr, arp_tproaddr, sizeof(arp_tproaddr));
+    u_int8_t arp_tproaddr[4] = {0xc0, 0xa8, 0x00, 0x01};
+    memcpy(arp.arp_tproaddr, arp_tproaddr, sizeof(arp_tproaddr));
 
     eth_arp_req req;
     req.eth = eth;
@@ -80,7 +73,6 @@ int main()
     {
         printf("arp send\n");
     }
-
     pcap_close(handle);
     return(0);
 }
